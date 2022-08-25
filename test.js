7117901088b868b0176956400c5b11688076e6be
ousscoder <its.mr.oussama@gmail.com>
2022-08-25 14:21:18 +0200

    CSSLAYOUT

---
+++ test.js
@@ -0,0 +1,40 @@
+function reverse(number) {
+
+
+    var a= number.reverse()
+     return a
+    
+}
+
+// console.log(reverse([1,2,3,4]))
+
+function sheep(a) {
+    var result=""
+
+    for (i=1;i<=a;i++) {
+
+       result+= result + i + " sheep "
+
+        
+    }
+        return result
+
+    }
+
+
+// console.log(sheep(55))
+
+
+
+function majuscule(a) {
+
+    var string=" oussama "
+    
+
+}
+
+// console.log(("a"))
+
+
+
+
